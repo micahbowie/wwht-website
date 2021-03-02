b84bf3fd258348e1cd05531e24fba519bae98453
Micah Bowie <micahbowie20@gmail.com>
2021-03-01 23:08:24 -0500

    adding imported packages to index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {
+  Provider } from 'react-redux'
+import
 
 ReactDOM.render(
   <React.StrictMode>
