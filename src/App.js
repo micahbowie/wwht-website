afa2fe1753cce432d34a5dceb2dac7c14f99597f
Micah Bowie <micahbowie20@gmail.com>
2021-03-01 23:03:12 -0500

    testing backend and fetching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 class App extends React.Component{
+
+  componentDidMount(){
+    fetch('http://localhost:3000/lists/1')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
   render() {
     return (
       <div className="App">
-
+        APP
       </div>
     );
 
