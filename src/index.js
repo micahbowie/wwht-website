b01039b5314b6d1c5a698616ce94a590739e6cf6
Micah Bowie <micahbowie20@gmail.com>
2021-03-01 23:26:24 -0500

    setting up index.js, importing redux, adding devtools enhancer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
-import {
-  Provider } from 'react-redux'
-import
+import { Provider } from 'react-redux'
+import App from './App';
+
+const enhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+let store = createStore(reducer, enhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
