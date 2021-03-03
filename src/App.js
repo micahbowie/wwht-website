180087237d7ada3508c485c5c30930728bc93abc
Micah Bowie <micahbowie20@gmail.com>
2021-03-03 13:57:09 -0500

    commented out unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { fetchUsers } from './actions/fetchUsers'
+// import { fetchUsers } from './actions/fetchUsers'
 import UsersContainer from './containers/usersContainer'
 
 
