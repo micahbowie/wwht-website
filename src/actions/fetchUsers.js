bcf8fcec8ae4767fd5b1782b41b87c294f59eccf
Micah Bowie <micahbowie20@gmail.com>
2021-03-03 13:57:38 -0500

    fixing fetch user so it dispatches to reducer

--- src/actions/fetchUsers.js
+++ src/actions/fetchUsers.js
@@ -1,6 +1,12 @@
 
 export function fetchUsers() {
-  // fetch('http://localhost:3000/users')
-  // .then(response => response.json())
-  // .then(data => console.log(data))
+  return(dispatch) => {
+    console.log("Inside fetch");
+    fetch('http://localhost:3000/users')
+    .then(response => response.json())
+    .then(users => dispatch({
+      type: 'FETCH_USERS',
+      payload: users
+    }))
+  }
 }
