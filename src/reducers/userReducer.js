5bfa8ed58f1a413c7c51b7acb8287794dd90da03
Micah Bowie <micahbowie20@gmail.com>
2021-03-03 13:58:55 -0500

    created switch statement for reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,3 +1,9 @@
 export default function userReducer(state = {users: []}, action) {
-
+debugger;
+  switch(action.type) {
+    case "FECTH_USERS":
+      return state
+    default:
+      return state
+  }
 }
