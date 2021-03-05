cbe4caf99fb89cbd330c26e13ebb1598ed771f43
Micah Bowie <micahbowie20@gmail.com>
2021-03-04 21:18:09 -0500

    imported files. created handleSubmit function. setup conncet

--- src/components/UsersForm.js
+++ src/components/UsersForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-
+import {connect} from 'react-redux';
+import {postUser} from '../actions/postUser';
 class UserForm extends React.Component {
 
   state ={name: '', email: '' }
@@ -11,19 +11,25 @@ class UserForm extends React.Component {
     })
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.postUser(this.state)
+  }
+
   render () {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
         <h1> Create an account: </h1>
           <label>what is your name?</label>
           <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/><br/>
           <label>What is your email?</label>
           <input type='text' placeholder='Email'value={this.state.email}name="email" onChange={this.handleChange}/><br/>
+          <input type='submit'/>
         </form>
       </div>
     )
   }
 }
 
-export default UserForm
+export default connect(null, {postUser})(UserForm)
