da5097f765fd4e042c4485246bbcbcbecaa8f6af
Micah Bowie <micahbowie20@gmail.com>
2021-03-04 21:48:38 -0500

    added a new case to reducer to accept the new user

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -5,6 +5,8 @@ export default function userReducer(state = {users: []}, action) {
   switch (action.type) {
     case 'FETCH_USERS':
       return {users: action.payload }
+      case 'POST_USER':
+        return {...state, users: [...state.users, action.payload] }
     default:
       return state
   }
