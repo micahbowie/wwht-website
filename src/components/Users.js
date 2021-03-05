75edff36175685052712c33dba840c5e1bd659cc
Micah Bowie <micahbowie20@gmail.com>
2021-03-04 20:16:12 -0500

    Iterating over props from store in Users component.

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,7 +4,9 @@ import React from 'react';
 const Users = (props) => {
   return (
     <div>
-      Users
+      {props.users.map(user =>
+      <li key={user.id}>{user.name} - {user.email}</li>
+      )}
     </div>
   )
 }
