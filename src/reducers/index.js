9d8eef07c7663fc860f2b5b16062c0ba4d89e3a8
Micah Bowie <micahbowie20@gmail.com>
2021-03-07 16:24:54 -0500

    added list reducer to reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import termsReducer from './termsReducer'
 import usersReducer from './userReducer'
 import jokeReducer from './jokeReducer'
+import listsReducer from './listsReducer'
 
 export default combineReducers({
   termsReducer,
   usersReducer,
-  jokeReducer
+  jokeReducer,
+  listsReducer
 })
